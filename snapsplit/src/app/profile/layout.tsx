82606315a8ed27d9bb69671fc6fdb8aa131cd23c
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-07-21 16:06:05 +0900

    chore: 기존 next-auth 로그인 로직 삭제

--- snapsplit/src/app/profile/layout.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import { SessionProvider } from 'next-auth/react';
-
-export default function ProfileLayout({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
--- snapsplit/src/shared/components/KakaoLoginButton.tsx
+++ snapsplit/src/shared/components/KakaoLoginButton.tsx
@@ -1,20 +1,12 @@
 'use client';
 
-import { signIn } from 'next-auth/react';
-
 interface KakaoLoginButtonProps {
   className?: string;
 }
 
 const KakaoLoginButton = ({ className = '' }: KakaoLoginButtonProps) => {
-  const handleKakaoLogin = async () => {
-    // 자동 리다이렉트 off + 카카오 인증 페이지로 즉시 이동
-    const res = await signIn('kakao', {
-      redirect: false,
-      // 카카오 로그인 후 리다이렉트할 URL
-      callbackUrl: '/profile',
-    });
-    console.log('Kakao login response:', res);
+  const handleKakaoLogin = () => {
+    console.log('카카오 로그인 버튼 클릭');
   };
 
   return (
