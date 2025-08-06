4e246a84fb2afed24e2325d858b6c45732f5a5b1
Emithen <86219540+Emithen@users.noreply.github.com>
2025-08-06 21:59:07 +0900

    feat: 결제/정산자 체크 상호작용

--- snapsplit/src/features/trip/[tripId]/budget/spend/_components/spendForm/payment/PaymentRow.tsx
+++ snapsplit/src/features/trip/[tripId]/budget/spend/_components/spendForm/payment/PaymentRow.tsx
@@ -1,17 +1,26 @@
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
 
 type Props = {
     name: string;
 }
 
 export default function PaymentRow({ name }: Props) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const toggleCheck = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
     <div className="flex items-center justify-between w-full py-2">
       <div>{name}</div>
       <div className="flex items-center">
         <div className="flex items-center justify-center w-20">
-          <button className="flex items-center justify-center w-6 h-6 rounded-full border-[1px] border-grey-350">
-            <Image src="/svg/check-spend-grey.svg" alt="" width={24} height={24} />
+          <button onClick={toggleCheck} className={`flex items-center justify-center w-6 h-6 rounded-full ${isChecked ? 'bg-primary text-white' : 'border-[1px] border-grey-250'}`}>
+            <Image src={isChecked ? "/svg/check-spend-white.svg" : "/svg/check-spend-grey.svg"} alt="" width={24} height={24} />
           </button>
         </div>
         <input type="text" className="flex items-center justify-center w-20 text-center text-grey-450" placeholder="0" />
