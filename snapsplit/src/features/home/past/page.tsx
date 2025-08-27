53b42a1af1888d10bc70afd95000f3a215d224be
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-08-27 21:54:18 +0900

    chore: 백엔드 리스폰스 응답에 따라 데이터 수정

--- snapsplit/src/features/home/past/page.tsx
+++ snapsplit/src/features/home/past/page.tsx
@@ -3,16 +3,12 @@ import PastTripList from './_components/PastTripList';
 import PastTripSummary from './_components/PastTripSummary';
 import mock from '@public/mocks/past-trip-mock.json';
 
-// 임시 데이터
-const tripCount = mock.length;
-const countryCount = new Set(mock.map((trip: any) => trip.country)).size;
-
 export default function PastTripPage() {
   return (
     <div className="flex flex-col px-5 pb-6 min-h-[100dvh] bg-light_grey overflow-auto">
       <PastTripHeader />
-      <PastTripSummary tripCount={tripCount} countryCount={countryCount} />
-      <PastTripList trips={mock} />
+      <PastTripSummary totalTrips={mock.data.totalTrips} totalCountries={mock.data.totalCountries} />
+      <PastTripList trips={mock.data.trips} />
     </div>
   );
 }
