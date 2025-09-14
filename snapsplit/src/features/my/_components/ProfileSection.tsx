4b6e8706d3e6de6a2317bfeb59c46277efc086d4
Emithen <86219540+Emithen@users.noreply.github.com>
2025-09-15 02:57:20 +0900

    fix: 상태 업데이트 시점 변경

--- snapsplit/src/features/my/_components/ProfileSection.tsx
+++ snapsplit/src/features/my/_components/ProfileSection.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { updateMyData } from '@/features/my/api/my-api';
 
 type ProfileSectionProps = {
@@ -14,6 +14,14 @@ export default function ProfileSection({ name, profileImage, userCode }: Profile
   const [preview, setPreview] = useState<string | null>(null);
   const [profileName, setProfileName] = useState<string>(name);
   const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  // 메모리 해제
+  useEffect(() => {
+    if (!preview) return;
+    return () => {
+      URL.revokeObjectURL(preview);
+    };
+  }, [preview]);
 
   const onPick = () => {
     fileRef.current?.click();
@@ -39,16 +47,15 @@ export default function ProfileSection({ name, profileImage, userCode }: Profile
   };
 
   const onSubmit = async () => {
-    setIsEditing(false);
-    setPreview(null);
-    setFile(null);
     if (!profileName && !file) return alert('이름과 프로필 이미지 중 하나는 필수입니다.');
-
     try {
       await updateMyData({
         name: profileName || undefined,
         profileImage: file || undefined,
       });
+      setIsEditing(false);
+      setPreview(null);
+      setFile(null);
     } catch (e) {
       console.error('프로필 업데이트 실패:', e);
     } finally {
@@ -69,14 +76,28 @@ export default function ProfileSection({ name, profileImage, userCode }: Profile
             priority
           />
         </div>
-        <button onClick={!preview ? onPick : onSubmit} className="absolute bottom-0 right-0 w-6 h-6 rounded-full overflow-hidden">
-          <Image src={!preview ? "/svg/edit-green.svg" : "/svg/check-green.svg"} alt="profile" width={100} height={100} className="object-cover" />
+        <button
+          onClick={!preview ? onPick : onSubmit}
+          className="absolute bottom-0 right-0 w-6 h-6 rounded-full overflow-hidden"
+        >
+          <Image
+            src={!preview ? '/svg/edit-green.svg' : '/svg/check-green.svg'}
+            alt="profile"
+            width={100}
+            height={100}
+            className="object-cover"
+          />
         </button>
 
         <input type="file" accept="image/*" ref={fileRef} onChange={onFileChange} className="hidden" />
       </div>
       <div className="flex flex-col items-center gap-1">
-        <input value={profileName} onChange={(e) => setProfileName(e.target.value)} className="text-head-0 text-center" disabled={!isEditing}/>
+        <input
+          value={profileName}
+          onChange={(e) => setProfileName(e.target.value)}
+          className="text-head-0 text-center"
+          disabled={!isEditing}
+        />
         <p className="text-body-3 text-green">{userCode}</p>
       </div>
     </div>
