e00e3310bd0499dba6e0f288ee6add7cc5f65c97
jsomnium <jsomnium@naver.com>
2025-09-22 16:14:20 +0900

    fix: 주석 삭제

--- snapsplit/src/features/home/api/home-api.ts
+++ snapsplit/src/features/home/api/home-api.ts
@@ -17,16 +17,10 @@ export const joinTripByCode = async (inviteCode: string): Promise<void> => {
     alert('여행에 성공적으로 참여했습니다!');
     return;
   } catch (error) {
-    // 4xx, 5xx 에러는 모두 여기서 처리합니다.
     if (axios.isAxiosError(error) && error.response?.data) {
       alert(error.response.data.message || '알 수 없는 서버 오류가 발생했습니다.');
       throw new Error(error.response.data.message || '알 수 없는 서버 오류가 발생했습니다.');
     }
-
-    // if (error instanceof Error) {
-    //   throw error; // 원래 에러를 그대로 던집니다.
-    // }
-
-    // throw new Error('여행 참여 중 알 수 없는 오류가 발생했습니다.');
+    throw new Error('여행 참여 중 알 수 없는 오류가 발생했습니다.');
   }
 };
