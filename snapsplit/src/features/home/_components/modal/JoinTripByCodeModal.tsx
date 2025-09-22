f0d4e2ee6d248ae2520d8611c8b121775b30acad
jsomnium <jsomnium@naver.com>
2025-09-22 16:00:45 +0900

    fix: 에러 메세지 수정

--- snapsplit/src/features/home/_components/modal/JoinTripByCodeModal.tsx
+++ snapsplit/src/features/home/_components/modal/JoinTripByCodeModal.tsx
@@ -40,7 +40,6 @@ export default function JoinTripByCodeModal({ onClose }: JoinTripByCodeModalProp
             alert('여행에 성공적으로 참여했습니다!');
           } catch (error) {
             console.error('여행 참여 실패:', error);
-            alert('유효하지 않은 코드입니다.\n다시 시도해주세요.');
           }
         }}
       >
