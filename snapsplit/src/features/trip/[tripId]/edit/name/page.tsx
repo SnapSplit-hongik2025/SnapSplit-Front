fb33bedf38eadc9d30ee653f3356826dbec95417
jsomnium <jsomnium@naver.com>
2025-10-26 15:32:10 +0900

    fix: 여행 편집 빌드 에러 수정

--- snapsplit/src/features/trip/[tripId]/edit/name/page.tsx
+++ snapsplit/src/features/trip/[tripId]/edit/name/page.tsx
@@ -4,18 +4,30 @@ import InputTripNameSection from '@/shared/components/steps/Step4_InputTripName'
 import { EditNamePageProps } from './type';
 
 import mock from '@public/mocks/edit-name-mock.json';
+import { useState } from 'react';
 
 const EditNamePage = ({ tripId }: EditNamePageProps) => {
   console.log('- tripId: ' + tripId);
 
+  const [tripName, setTripName] = useState<string>('');
+  const [tripImageUrl, setTripImageUrl] = useState<string | null>(null);
+  const [tripImageFile, setTripImageFile] = useState<File | null>(null);
+
+  console.log('- tripName: ' + tripName);
+  console.log('- tripImageUrl: ' + tripImageUrl);
+  console.log('- tripImageFile: ' + tripImageFile);
+
   return (
     <InputTripNameSection
       tripName={mock.data.tripName}
-      tripImage={mock.data.tripImage}
+      tripImageUrl={mock.data.tripImage}
       onClick={() => {
         /* api 호출 */
       }}
       variant="edit"
+      setTripName={setTripName}
+      setTripImageUrl={setTripImageUrl}
+      setTripImageFile={setTripImageFile}
     />
   );
 };
