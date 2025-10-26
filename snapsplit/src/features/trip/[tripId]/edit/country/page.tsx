46ac11211ceb75d88fd0c8e7a4911eb6ef81ea0f
jsomnium <jsomnium@naver.com>
2025-10-26 16:36:40 +0900

    feat: 수정전 여행지 불러오기 api 연동 (2)

--- snapsplit/src/features/trip/[tripId]/edit/country/page.tsx
+++ snapsplit/src/features/trip/[tripId]/edit/country/page.tsx
@@ -1,20 +1,46 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 import CountrySearchSection from '@/shared/components/steps/Step1_CountrySearch';
 import { EditCountryPageProps } from './type';
 import { Country } from '@/shared/types/country';
-import mock from '@public/mocks/edit-country-mock.json';
+import { useQuery } from '@tanstack/react-query';
+import { GetCountryTripDto } from '@/features/trip/createTrip/types/type';
+import { getTripCountries } from '../api/edit-trip-api';
 
 const EditCountryPage = ({ tripId }: EditCountryPageProps) => {
-  console.log('tripId : ' + tripId);
   const router = useRouter();
 
+  const [selected, setSelected] = useState<Country[]>([]);
+
+  const { data, isLoading, isError, error, isSuccess } = useQuery<GetCountryTripDto, Error>({
+    queryKey: ['countryTrip', tripId],
+    queryFn: () => getTripCountries(tripId),
+    enabled: !!tripId,
+  });
+
+  useEffect(() => {
+    if (data && data.selectedCountries) {
+      setSelected(data.selectedCountries);
+    }
+  }, [data]);
+
+  if (isLoading) {
+    return <div>여행지 정보를 불러오는 중입니다...</div>;
+  }
+
+  if (isError) {
+    return <div>오류가 발생했습니다: {error.message}</div>;
+  }
+
+  if (!data || !isSuccess) {
+    return <div>여행지 정보가 없습니다.</div>;
+  }
+
   // mock 데이터
-  const countries: Country[] = mock.data.countries;
-  const [selected, setSelected] = useState<Country[]>(mock.data.selectedCountries);
+  const countries: Country[] = data.countries;
 
   // 토글 핸들러
   const toggleCountry = (country: Country) => {
