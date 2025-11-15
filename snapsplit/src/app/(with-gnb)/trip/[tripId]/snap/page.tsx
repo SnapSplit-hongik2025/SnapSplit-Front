7317ee92945d93dd3ebcd961bdc838a2e23bf78c
Emithen <86219540+Emithen@users.noreply.github.com>
2025-11-15 15:50:38 +0900

    fix: page props 주입 수정

--- snapsplit/src/app/(with-gnb)/trip/[tripId]/snap/page.tsx
+++ snapsplit/src/app/(with-gnb)/trip/[tripId]/snap/page.tsx
@@ -1,5 +1,7 @@
 import SnapPage from '@/features/trip/[tripId]/snap/SnapPage';
 
-export default function Page({ params }: { params: { tripId: string } }) {
-  return <SnapPage tripId={params.tripId} />;
+export default async function Snap({ params }: { params: Promise<{ tripId: string }> }) {
+  const { tripId } = await params;
+
+  return <SnapPage tripId={tripId} />;
 }
