70580a38cd9268311413365dc37627980a535455
jsomnium <jsomnium@naver.com>
2025-11-27 01:12:39 +0900

    feat: primary 색상 카테고리 아이콘 여행홈에 추가

--- snapsplit/src/features/trip/[tripId]/budget/_components/ExpenseItem.tsx
+++ snapsplit/src/features/trip/[tripId]/budget/_components/ExpenseItem.tsx
@@ -2,6 +2,8 @@ import { ExpenseDto } from '../types/budget-dto-type';
 import { routerPath } from '@/shared/constants/routePath';
 import { getSymbol } from '@/shared/utils/currency';
 import Link from 'next/link';
+import Image from 'next/image';
+import { EXPENSE_CATEGORY } from '@/shared/constants/expense';
 
 type ExpenseItemProps = {
   expense: ExpenseDto;
@@ -16,18 +18,26 @@ const ExpenseItem = ({ expense, tripId }: ExpenseItemProps) => {
 
   const symbol = getSymbol(expense.currency);
 
+  const categoryIcon = EXPENSE_CATEGORY.find((item) => item.backendName === expense.category);
+  const iconSrc = categoryIcon?.primary || '/svg/category-others-primary.svg';
+  const iconAlt = categoryIcon?.name || '기타';
+
   return (
     <Link href={ExpenseDetailPath} className="w-full flex gap-2 py-4">
-      <div className="w-10 h-10 min-w-10 min-h-10 bg-grey-250 rounded-xl" />
-      <div className="w-full flex-col">
-        <div className="flex justify-between text-label-1">
-          <p>{expense.expenseName}</p>
+      <div className="relative w-10 h-10 min-w-10 min-h-10 flex items-center justify-center">
+        <Image src={iconSrc} alt={iconAlt} width={28} height={28} />
+      </div>
+      <div className="w-full flex justify-between">
+        <div className="flex flex-col justify-center">
+          <div className="flex justify-between text-label-1">
+            <p>{expense.expenseName}</p>
+          </div>
+          {expense.expenseMemo && <p className="text-caption-1 text-grey-550">{expense.expenseMemo}</p>}
+        </div>
+        <div className="flex flex-col items-end justify-center">
           <p>
             {symbol} {expense.amount.toLocaleString()}
           </p>
-        </div>
-        <div className="flex justify-between text-label-1">
-          <p className="text-caption-1 text-grey-550">{expense.expenseMemo}</p>
           <p className="text-caption-1 text-grey-550">{userNames}</p>
         </div>
       </div>
